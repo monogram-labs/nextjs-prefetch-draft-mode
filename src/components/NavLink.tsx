c876ffefda07a7ef10e7218bbb8b7c4f58c6cfd3
Chalo Salvador <chalo@monogram.io>
2024-01-24 08:22:57 +0100

    Add NavLink component and enable draft mode

---
+++ src/components/NavLink.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { draftMode } from "next/headers";
+import Link from "next/link";
+
+export default function NavLink() {
+	draftMode().enable();
+	
+	return (
+		<Link href={'/about'}>Link</Link>
+	)
+}
